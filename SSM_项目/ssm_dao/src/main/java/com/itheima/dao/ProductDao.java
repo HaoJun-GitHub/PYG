4a2fd305418db50697c4910afb1a6b062fca856d
HaoJun@Git <xiehaojun@outlook.com>
2019-02-27 11:17:08 +0800

    添加了ssm——项目

---
+++ SSM_项目/ssm_dao/src/main/java/com/itheima/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package com.itheima.dao;
+
+/**
+ * Created by HaoJun on 2019/2/22 11:35
+ *
+ * @author:豪俊
+ * @version:V1.0 “生于忧患，死于安乐。”
+ * @Description:
+ */
+public interface ProductDao {
+
+}
